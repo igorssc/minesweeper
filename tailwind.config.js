b2af68a8ba8a5cc73b2e799a52001c770067ed66
Igor Costa <igorsantoscosta@gmail.com>
2024-08-14 02:29:18 -0300

    feat: add new background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,18 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        thelamonblack: ['thelamonblack', 'sans-serif'],
+        'bleeding-cowboys': ['bleeding-cowboys', 'sans-serif']
+      },
+      backgroundImage: {
+        background: "url('/images/background.png')"
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
