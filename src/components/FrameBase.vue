b3b7b23afb8b0b08852244fb56aac67b2f169673
Igor Costa <igorsantoscosta@gmail.com>
2024-09-11 19:24:47 -0300

    refactor: edit colors of game

--- src/components/FrameBase.vue
+++ src/components/FrameBase.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main
-    class="bg-gray-900 border-2 border-gray-950 border-dashed bg-opacity-90 flex gap-2 flex-col justify-center items-center h-fit p-4 rounded-md max-w-[calc(100vw-4rem)] overflow-x-auto"
+    class="bg-zinc-950 border-2 border-zinc-950 border-dashed bg-opacity-90 flex gap-2 flex-col justify-center items-center h-fit p-4 rounded-md max-w-[calc(100vw-4rem)] overflow-x-auto"
   >
     <slot />
   </main>
--- src/components/LevelButton.vue
+++ src/components/LevelButton.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <button class="text-gray-400 bg-gray-950/70 px-8 py-2 rounded-md hover:bg-gray-950">
+  <button
+    class="text-zinc-400 border-2 border-transparent bg-zinc-900 px-8 py-2 rounded-md hover:bg-transparent hover:border-zinc-700"
+  >
     <slot />
   </button>
 </template>
