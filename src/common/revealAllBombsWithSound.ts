59c638d888c897a200f949fcd6182ebe09327950
Igor Costa <igorsantoscosta@gmail.com>
2024-09-12 22:48:17 -0300

    fix: increase bomb volume

--- src/common/revealAllBombsWithSound.ts
+++ src/common/revealAllBombsWithSound.ts
@@ -29,7 +29,7 @@ export const revealAllBombsWithSound = ({
 }: RevealAllBombsWithSoundProps) => {
   const bombSoundHowl = new Howl({
     src: [bombSound],
-    volume: 0.0015
+    volume: 0.005
   })
 
   hasSound.value && bombSoundHowl.play()
