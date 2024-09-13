2ce251fce5414f120d081e56e28ac74fffd3bff6
Igor Costa <igorsantoscosta@gmail.com>
2024-09-12 23:17:09 -0300

    refactor: improving usage metrics

--- src/common/calculatePerformance.ts
+++ src/common/calculatePerformance.ts
@@ -1,6 +1,6 @@
 import type { Ref } from 'vue'
 import { calculateBombsScore } from './calculateBombsScore'
-import type { BoardItemProps } from '@/enums/cellState'
+import { CELL_STATE, type BoardItemProps } from '@/enums/cellState'
 
 type PerformanceMetrics = {
   elapsedTime: Ref<number>
@@ -33,16 +33,18 @@ export const calculatePerformance = ({
   const maxClicks = rows.value * columns.value // Máximo teórico de cliques (um por célula)
 
   // Pesos para cada métrica
-  const timeWeight = 0.2
-  const clickWeight = 0.2
-  const moveWeight = 0.2
-  const bombsWeight = 0.2
-  const victoryWeight = 0.1
+  const timeWeight = isVictory.value || isGameOver.value ? 0.2 : 0.25
+  const clickWeight = isVictory.value || isGameOver.value ? 0.2 : 0.25
+  const moveWeight = isVictory.value || isGameOver.value ? 0.2 : 0.25
+  const bombsWeight = isVictory.value || isGameOver.value ? 0.2 : 0.25
+  const victoryWeight = 0.2
   const lossWeight = 0.1
+  const gameOverPenaltyWeight = 0.1
 
   // Normalizando as métricas
   const timeScore = Math.max(0, 100 - (elapsedTime.value / maxTime) * 100)
   const clickScore = Math.max(0, 100 - (clicksCount.leftCursor / maxClicks) * 100)
+
   const maxPossibleClicks = rows.value * columns.value
   const moveScore = Math.max(
     0,
@@ -54,7 +56,18 @@ export const calculatePerformance = ({
   )
   const bombsScore = Math.min(calculateBombsScore({ baseBoard, boardDisplayed }), 100)
   const victoryScore = isVictory.value ? 100 : 0
+
+  // Penalidade por jogo perdido
   const lossPenalty = isGameOver.value ? 100 : 0
+
+  // Métrica para penalização de game over
+  const totalNonBombCells = baseBoard.value.flat().filter((cell) => cell !== CELL_STATE.BOMB).length
+  const openedCells = boardDisplayed.value
+    .flat()
+    .filter((cell) => cell !== CELL_STATE.FLAG && cell !== CELL_STATE.BOMB).length
+  const efficiencyScore = isGameOver.value
+    ? Math.max(0, 100 - (100 * (totalNonBombCells - openedCells)) / totalNonBombCells)
+    : 0
 
   // Calculando contribuições ponderadas
   const weightedTimeScore = timeScore * timeWeight
@@ -63,15 +76,15 @@ export const calculatePerformance = ({
   const weightedBombsScore = bombsScore * bombsWeight
   const weightedVictoryScore = victoryScore * victoryWeight
   const weightedLossPenalty = lossPenalty * lossWeight
+  const weightedGameOverPenalty = efficiencyScore * gameOverPenaltyWeight
 
-  let totalScore =
-    weightedTimeScore + weightedClickScore + weightedMoveScore + weightedBombsScore + 20
+  let totalScore = weightedTimeScore + weightedClickScore + weightedMoveScore + weightedBombsScore
 
   if (isVictory.value) totalScore += weightedVictoryScore
-  if (isGameOver.value) totalScore -= weightedLossPenalty
+  if (isGameOver.value) totalScore -= weightedLossPenalty + weightedGameOverPenalty
 
-  // Calculando a pontuação total ponderada
-  totalScore = Math.max(0, Math.min(100, totalScore))
+  // Garantindo que a pontuação total não ultrapasse 100
+  totalScore = Math.max(0, Math.min(totalScore, 100))
 
   return totalScore
 }
