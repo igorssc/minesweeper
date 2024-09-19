73fa34ce983fcc3475c68613c7b5a101233d6e2a
Igor Costa <igorsantoscosta@gmail.com>
2024-09-19 10:38:33 -0300

    feat: create service worker

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url)
+
+  // Se o arquivo for o GIF com o parâmetro ?t=...
+  if (url.pathname.endsWith('bomb_compressed.gif')) {
+    // Remove o parâmetro para buscar o cache
+    url.search = '' // Remove os parâmetros de cache busting
+
+    event.respondWith(
+      caches.match(url).then((response) => {
+        if (response) {
+          // Se o arquivo já está no cache, o retorna
+          return response
+        }
+
+        // Se não está no cache, busca na rede
+        return fetch(event.request).then((networkResponse) => {
+          // Opcionalmente, adiciona a resposta ao cache
+          return caches.open('dynamic-cache').then((cache) => {
+            cache.put(url, networkResponse.clone())
+            return networkResponse
+          })
+        })
+      })
+    )
+  } else {
+    // Processa outros arquivos normalmente
+    event.respondWith(fetch(event.request))
+  }
+})
