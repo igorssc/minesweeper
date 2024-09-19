161337dc797ffae5715e4c2af00c25d8d7bb45e4
Igor Costa <igorsantoscosta@gmail.com>
2024-09-18 21:20:32 -0300

    feat: implement pwa

--- src/main.ts
+++ src/main.ts
@@ -24,3 +24,16 @@ app.use(router)
 app.component('VueModalityV3', VueModalityV3)
 
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('ServiceWorker registrado com sucesso:', registration)
+      })
+      .catch((error) => {
+        console.log('Falha ao registrar o ServiceWorker:', error)
+      })
+  })
+}
