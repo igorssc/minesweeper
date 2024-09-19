bb7405ea59f08afea8a11de549d571470145729f
Igor Costa <igorsantoscosta@gmail.com>
2024-09-18 22:15:42 -0300

    chore: edit service worker

--- src/main.ts
+++ src/main.ts
@@ -24,16 +24,3 @@ app.use(router)
 app.component('VueModalityV3', VueModalityV3)
 
 app.mount('#app')
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        console.log('ServiceWorker registrado com sucesso:', registration)
-      })
-      .catch((error) => {
-        console.log('Falha ao registrar o ServiceWorker:', error)
-      })
-  })
-}
